e0093359833df532d94218e5577690fd718d2ca9
lcamarda <camarda.luca@gmail.com>
2019-10-09 16:05:30 +0000

    modified:   logical_switch.tf

--- terraform/logical_switch.tf
+++ terraform/logical_switch.tf
@@ -14,3 +14,20 @@ resource "nsxt_logical_switch" "web" {
     }
 }
 
+resource "nsxt_logical_switch" "app" {
+  admin_state       = "UP"
+  description       = "LS created by Terraform"
+  display_name      = "OV-App-Terraform-${var.tenant_name}"
+  transport_zone_id = "${data.nsxt_transport_zone.overlay_tz.id}"
+  replication_mode  = "MTEP"
+  tag {
+        scope = "tenant"
+        tag = "${var.tenant_name}"
+    }
+  tag {
+        scope = "CreatedBy"
+        tag = "Terraform"
+    }
+}
+
+
